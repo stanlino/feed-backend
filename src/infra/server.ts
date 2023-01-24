ec6a2666fd315289aa4cd7e611c87d1b718c1668
Stanley Filipe Neiva <stanley92filipe@gmail.com>
2023-01-24 19:27:15 -0300

    docs(docs): add a swagger documentation

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -4,12 +4,18 @@ import "dotenv/config"
 import "express-async-errors"
 
 import express from 'express'
+import swaggerUi from 'swagger-ui-express';
+
 import { routes } from "./routes";
 import { expressAsyncErrors } from "./middlewares/expressAsyncErrors";
+
+import openapi from './openapi.json'
 
 const server = express()
 
 server.use(express.json())
+
+server.use('/', swaggerUi.serve, swaggerUi.setup(openapi))
 
 server.use(routes)
 

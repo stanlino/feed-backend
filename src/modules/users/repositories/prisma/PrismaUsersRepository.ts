d201bf68a0a63cc0f2dbdba7ae0b4fffec5a5c05
Stanley Filipe Neiva <stanley92filipe@gmail.com>
2023-01-24 14:33:46 -0300

    feat(users): prisma users repository

---
+++ src/modules/users/repositories/prisma/PrismaUsersRepository.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+import { randomUUID } from "crypto";
+import { CreateUserDto } from "../../dtos/createUserDto";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class PrismaUserRepository implements IUsersRepository {
+
+  private prisma: PrismaClient
+
+  constructor() {
+    this.prisma = new PrismaClient()
+  }
+
+  async create({ username, password }: CreateUserDto): Promise<void> {
+    await this.prisma.user.create({
+      data: {
+        id: randomUUID(),
+        username,
+        password
+      }
+    })
+  }
+
+  async findOne(username: string): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        username
+      }
+    })
+
+    return user
+  }
+
+  async delete(username: string): Promise<void> {
+    await this.prisma.user.delete({
+      where: {
+        username
+      }
+    })
+  }
+
+}

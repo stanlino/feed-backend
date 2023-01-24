ad16a8377e954ec1589d8c67890f61a5b2b6cd01
Stanley Filipe Neiva <stanley92filipe@gmail.com>
2023-01-24 16:10:04 -0300

    feat(infra): add middleware to handle with errors

---
+++ src/infra/middlewares/expressAsyncErrors.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
+import { Exception } from "../../errors/Exception";
+
+export function expressAsyncErrors(error: Error, request: Request, response: Response, next: NextFunction) {
+  if (error instanceof Exception) {
+    return response.status(error.statusCode).json(error)
+  }
+
+  if (error instanceof ZodError) {
+    return response.status(400).json(error)
+  }
+
+  return response.status(400).json(error)
+}
--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,15 +1,19 @@
 import "reflect-metadata";
 import "../container"
 import "dotenv/config"
+import "express-async-errors"
 
 import express from 'express'
 import { routes } from "./routes";
+import { expressAsyncErrors } from "./middlewares/expressAsyncErrors";
 
 const server = express()
 
 server.use(express.json())
 
 server.use(routes)
+
+server.use(expressAsyncErrors)
 
 export {
   server

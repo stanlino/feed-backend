b9cc13f65fc4ae5c8de29d33dee37d5531084b02
Stanley Filipe Neiva <stanley92filipe@gmail.com>
2023-01-24 15:49:32 -0300

    feat(infra): add middleware to authenticate user by access token

--- src/infra/routes/users.routes.ts
+++ src/infra/routes/users.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 
 import { UsersController } from "../../modules/users/controllers/UsersController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const usersController = new UsersController()
 
 const usersRoutes = Router()
 
 usersRoutes.post('/create', usersController.create)
-usersRoutes.post('/delete', usersController.delete)
+usersRoutes.post('/delete', ensureAuthenticated, usersController.delete)
 
 export {
   usersRoutes
